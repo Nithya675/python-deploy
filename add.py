57bcdc9e12e16f80f9a0fd153d867232980648eb
Nithya675 <nithyasrichittachitta@gmail.com>
2025-06-03 17:13:57 -0500

    Create add.py

---
+++ add.py
@@ -0,0 +1,12 @@
+# simple_script.py
+
+# Print a welcome message
+print("Hello from Python!")
+
+# Add two numbers
+a = 5
+b = 3
+sum = a + b
+
+# Display the result
+print("The sum of", a, "and", b, "is", sum)
